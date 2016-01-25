ae76b6a4808737027ae2316339ed99a1e14d82d9
Jason Narvaez <thejason@linux2.cs.tamu.edu>
2016-01-25 13:10:48 -0600

    parsing completed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <regex>
+#include <stdio.h>
 
 using namespace std;
 
@@ -13,75 +15,77 @@ int main(){
 	int GameID;
 	int AchievementID;
 	int PlayerID;
-	cout<<"Reading in input..."<<endl;
+	int AchievementValue;
+	cout << "Reading in input..." << endl;
 	while(cin.good()){
-		cin>>command;
-		cout<<"command: "<<command<<endl;
+		cin >> command;
+		cout << "command: " << command << endl;
 		
 		if(command == "AddGame"){
-			cin>>GameID;
+			cin >> GameID;
 			getline(cin,GameName);
-			cout<<command<<" "<<GameID<<" "<<GameName<<endl;
+			cout << command << " " << GameID << " " << GameName << endl;
 		}
 		else if(command == "AddAchievement"){
-			cin>>GameID>>AchievementID;
-			getline(cin,temp);
-			int pos1;
-			int pos2;
-			/* 
-			for(int i=0;i<temp.size();i++){
-				if(temp[i] == "\"" && i>0)
-					pos2 = i;
-				else if(temp[i] == "\""&& i==0)
-					pos1 = i;
-			}
-			AchievementName = temp.substr(pos1,pos2);
-			 */
+			string temp2;
+			
+			cin >> GameID >> AchievementID;
+			cout << command << " " << GameID << " " << AchievementID << endl; 
+			
+			getline(cin,temp,'\"');//gets and discards the first quotation mark
+			getline(cin,AchievementName,'\"');//read entire string before the next quotation mark
+			cin >> AchievementValue;
+			
+			cout << "The achievement: " << AchievementName << endl;
+			cout << "The achievement Value: " << AchievementValue << endl;
+			cout << command << " " << GameID << " " << AchievementID << " " << AchievementName << " " << AchievementValue<<endl;
+			cout << "--------------------------------------" << endl;
+			
 		}
 		else if(command == "AddPlayer"){
-			cin>>PlayerID;
+			cin >> PlayerID;
 			getline(cin,PlayerName);
-			cout<<command<<" "<<PlayerID<<" "<<PlayerName<<endl;
+			cout << command << " " << PlayerID << " " << PlayerName << endl;
 			
 		}
 		else if(command == "AddFriends"){
 			int Player1;
 			int Player2;
-			cin>>Player1>>Player2;
-			cout<<command<<" "<<Player1<<" "<<Player2<<endl;
+			cin >> Player1 >> Player2;
+			cout << command << " " << Player1 << " " << Player2 << endl;
 		}
 		else if(command == "Plays"){
-			cin>>PlayerID>>GameID;
+			cin >> PlayerID >> GameID;
 			getline(cin,IGN);
-			cout<<command<<" "<<PlayerID<<" "<<GameID<<" "<<IGN<<endl;
+			cout << command << " " << PlayerID << " " << GameID << " " << IGN << endl;
 		}
 		else if(command == "Achieve"){
 			cin>>PlayerID>>GameID>>AchievementID;
-			cout<<command<<" "<<PlayerID<<" "<<GameID<<" "<<AchievementID<<endl;
+			cout << command << " " << PlayerID << " " << GameID << " " << AchievementID << endl;
 		}
 		else if(command == "FriendsWhoPlay"){
-			cin>>PlayerID>>GameID;
-			cout<<command<<" "<<PlayerID<<" "<<GameID<<endl;
+			cin >> PlayerID >> GameID;
+			cout << command <<" "<< PlayerID <<" "<< GameID << endl;
 		}
 		else if(command == "ComparePlayers"){
 			int Player1;
 			int Player2;
-			cin>>Player1>>Player2;
-			cout<<command<<" "<<Player1<<" "<<Player2<<endl;
+			cin >> Player1 >> Player2;
+			cout << command << " " << Player1 << " " << Player2 << endl;
 		}
 		else if(command == "SummarizePlayer"){
-			cin>>GameID;
-			cout<<command<<" "<<GameID<<endl;
+			cin >> GameID;
+			cout << command << " " << GameID << endl;
 		}
 		else if(command == "SummarizeAchievement"){
-			cin>>GameID>>AchievementID;
-			cout<<command<<" "<<GameID<<" "<<AchievementID<<endl;
+			cin >> GameID >> AchievementID;
+			cout << command << " " << GameID << " " << AchievementID << endl;
 		}
 		else if(command == "AchievementRanking"){
-			cout<<command<<endl;
+			cout << command << endl;
 		}
 		else{
-			cout<<"command not recognized."<<endl;
+			cout << "command not recognized: " << command<< endl;
 		}
 		
 	}
--- makefile
+++ makefile
@@ -3,10 +3,10 @@
 all: main
 
 main.o: main.cpp
-	g++-4.7 -c -g main.cpp
+	g++-4.7 -std=c++11 -c -g main.cpp
 	
 main: main.o
-	g++-4.7 -g -o main main.o
+	g++-4.7 -std=c++11 -g -o main main.o
 	
 run:
 	./main
